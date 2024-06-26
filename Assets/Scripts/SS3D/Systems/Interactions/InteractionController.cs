c2310adb6e74aed8c2aa6f0cc52129139c7e7bc0
Necromunger <lil_tommy_tom@hotmail.com>
2024-06-26 15:27:00 +1000

    Drop Interactions and Item placement (#1446)
    * Cleaned up item placement, check for flat surface, keep rotation even
    
    * Added viewpoints to entities.
    
    - Added checks regarding dropping items to the angle of the surface they are placed on.
    - Simplified dropping items to only use the Y axis, so you can place items flatly but also the rotation you want.
    
    * Cleanup to only target world layer mask instead of character.
    
    Added check that test raycast hit is near interaction point.
    
    * cleanup
    
    * Polish the code
    
    ---------
    
    Co-authored-by: Mechar418 <iamteapot42@gmail.com>

--- Assets/Scripts/SS3D/Systems/Interactions/InteractionController.cs
+++ Assets/Scripts/SS3D/Systems/Interactions/InteractionController.cs
@@ -277,15 +277,17 @@ namespace SS3D.Systems.Interactions
 
             // Raycast to find target game object
             Vector3 point = Vector3.zero;
+            Vector3 normal = Vector3.zero;
             bool raycast = Physics.Raycast(ray, out RaycastHit hit, float.PositiveInfinity, _selectionMask, QueryTriggerInteraction.Ignore);
             if (raycast)
             {
                 point = hit.point;
+                normal = hit.normal;
                 GameObject target = hit.transform.gameObject;
                 targets = GetTargetsFromGameObject(source, target);
             }
 
-            interactionEvent = new InteractionEvent(source, targets[0], point);
+            interactionEvent = new InteractionEvent(source, targets[0], point, normal);
 
             return GetInteractionsFromTargets(source, targets, interactionEvent);
         }
